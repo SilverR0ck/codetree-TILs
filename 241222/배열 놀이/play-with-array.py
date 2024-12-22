0a1d330525523ac3192ad22bbb7f567a4e39b1a4
eunseok <126962205+SilverR0ck@users.noreply.github.com>
2024-12-22 21:34:10 +0900

    Title: 배열 놀이, Time: 51ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241222/배열 놀이/play-with-array.py
+++ 241222/배열 놀이/play-with-array.py
@@ -6,11 +6,12 @@ for i in range(q):
     if func[0] == 1:
         idx = func[1]-1
         print(l[idx])
+        
     elif func[0] == 2:
         if func[1] not in l:
             print("0")
         else:
-            l.index(func[1])
+            print(l.index(func[1])+1)
     else:
         for i in range(func[1]-1, func[2]):
             print(l[i], end=" ")
