cba26ddb7706f56d5fcc9121454bd78566b47763
eunseok <126962205+SilverR0ck@users.noreply.github.com>
2024-12-27 17:00:53 +0900

    Title: 배열의 평균, Time: 46ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241227/배열의 평균/ave-of-array.py
@@ -0,0 +1,37 @@
+row = 2
+col = 4
+
+arr = [
+    list(map(int, input().split()))
+    for _ in range(row)
+]
+
+r = []
+c = []
+t = []
+row_avg = 0.0
+col_avg = 0.0
+total_avg=0.0
+
+for i in range(row):
+    for j in range(col): #가로줄 평균
+        col_avg += arr[i][j]
+        total_avg += arr[i][j]
+    c.append(col_avg/4)
+    col_avg = 0
+    
+for j in range(col):
+    for i in range(row):
+        row_avg += arr[i][j]
+    r.append(row_avg/2)
+    row_avg = 0
+
+
+for i in c:
+    print(i, end=' ')
+    
+print()
+for i in r:
+    print(i, end=' ')
+print()
+print(total_avg/8)
