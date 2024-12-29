a6568f17565636598c00d2e7f167ef422a84df6b
eunseok <126962205+SilverR0ck@users.noreply.github.com>
2024-12-30 03:00:15 +0900

    Title: 최대공약수 구하기, Time: 81ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 241230/최대공약수 구하기/find-the-greatest-common-divisor.py
@@ -0,0 +1,13 @@
+def func(n, m):
+    i=2
+    arr = []
+    while(1):
+        if n%i==0 and m%i==0:
+            arr.append(i)
+        if i>=n or i>=m:
+            break
+        i+=1
+    print(max(arr))
+
+n, m = map(int, input().split())
+func(n, m)
